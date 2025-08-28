7c318390b1abf2c3ea65e7263092ea0bc042585b
Ronan <ronan@ronanmccoy.com>
2025-08-28 00:44:09 -0700

    add missing post-installation command for homebrew

--- scripts/mac_setup.sh
+++ scripts/mac_setup.sh
@@ -20,6 +20,13 @@
 if ! command -v brew &> /dev/null; then
 	echo "--> Homebrew is not installed. Let's try and install it..."
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+	# add brew to PATH
+	if [[ -f "/opt/homebrew/bin/brew" ]]; then
+    	eval "$(/opt/homebrew/bin/brew shellenv)"
+	elif [[ -f "/usr/local/bin/brew" ]]; then
+    	eval "$(/usr/local/bin/brew shellenv)"
+	fi
 else
 	echo "--> Homebrew is already installed."
 fi
